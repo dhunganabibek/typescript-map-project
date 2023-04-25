95d4c3cef33b8807cc2da6e1c073572fb65e2266
dhunganabibek <Dhunganabibek2016@gmail.com>
2023-04-25 00:38:59 -0500

    map api added

--- src/Company.ts
+++ src/Company.ts
@@ -1,14 +1,16 @@
 import { faker } from "@faker-js/faker";
 
-export class User {
-  name: string;
+export class Company {
+  companyName: string;
+  catchPhrase: string;
   location: {
     latitude: number;
     longitude: number;
   };
 
   constructor() {
-    this.name = faker.name.firstName();
+    this.companyName = faker.company.name();
+    this.catchPhrase = faker.company.catchPhrase();
     this.location = {
       latitude: parseFloat(faker.address.latitude()),
       longitude: parseFloat(faker.address.longitude()),
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hi there");
+/// <reference types="@types/google.maps" />
+
+import { User } from "./User";
+import { Company } from "./Company";
+
+console.log(document.getElementById("map"));
+
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 1,
+    lng: 1,
+  },
+});
