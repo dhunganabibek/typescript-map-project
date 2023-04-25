299ee8279d8af92706b6dae971dfbe1c665af867
dhunganabibek <Dhunganabibek2016@gmail.com>
2023-04-24 22:54:46 -0500

    created User class and faker library

--- src/User.ts
+++ src/User.ts
@@ -1,1 +1,17 @@
-class User {}
+import faker from "@faker-js/faker";
+
+class User {
+  name: string;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+
+  constructor() {
+    this.name = faker.faker.name.firstName();
+    this.location = {
+      latitude: parseFloat(faker.faker.address.latitude()),
+      longitude: parseFloat(faker.faker.address.longitude()),
+    };
+  }
+}
